eae71c4bccb2f8ee7a41d66168d47fb91ef77663
Roxanne Guo <roxane.guo@gmail.com>
2012-06-19 08:43:16 -0700

    making package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=open("README").read(),
     entry_points="""
     [console_scripts]
-    lava-serial-test=lava_serial_test.main:main
+    lava-serial-test=lava_serial_test.main:__main__
     """,
     classifiers=[
         "Development Status :: Pre - Alpha",
@@ -24,6 +24,7 @@ setup(
     install_requires=[
         'lava-tool >= 0.2',
         'versiontools >= 1.4',
+        'pexpect >= 2.4',
     ],
     setup_requires=[
         'versiontools >= 1.4'
